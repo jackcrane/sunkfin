1feff6ca9563d6c9d19fb1960bb249613be2536d
Jack Crane <jack@jackcrane.rocks>
2025-03-13 03:39:14 -0400

    done?

---
+++ Sunkfin/DownloadManager.swift
@@ -0,0 +1,8 @@
+//
+//  DownloadManager.swift
+//  Sunkfin
+//
+//  Created by Jack Crane on 3/13/25.
+//
+
+import Foundation
---
+++ Sunkfin/LibraryItemView.swift
@@ -0,0 +1,18 @@
+//
+//  LibraryItemView.swift
+//  Sunkfin
+//
+//  Created by Jack Crane on 3/13/25.
+//
+
+import SwiftUI
+
+struct LibraryItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LibraryItemView()
+}
