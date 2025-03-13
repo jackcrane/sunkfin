e94a20813263ccc9e8589c4e423dfb7954f57d42
Jack Crane <jack@jackcrane.rocks>
2025-03-13 05:27:18 -0400

    ugh

---
+++ Pods/Target Support Files/Pods-Sunkfin-SunkfinUITests/Pods-Sunkfin-SunkfinUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Sunkfin_SunkfinUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Sunkfin_SunkfinUITests
+@end
---
+++ Pods/Target Support Files/Pods-SunkfinTests/Pods-SunkfinTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SunkfinTests : NSObject
+@end
+@implementation PodsDummy_Pods_SunkfinTests
+@end
